ed3663b7c94ba4115a0bee209f5fa4eaed81035f
franciscaserrao <francisca.s.serrao@gmail.com>
2024-03-18 12:23:48 +0000

    updated links

--- src/App.js
+++ src/App.js
@@ -9,15 +9,27 @@ export default function App() {
       <Form />
       <p>
         Coded by{" "}
-        <a href="https://github.com/franciscaserrao" target="_blank">
+        <a
+          href="https://github.com/franciscaserrao"
+          target="_blank"
+          rel="noreferrer"
+        >
           Francisca Serrão
         </a>
         , open-sourced on{" "}
-        <a href="https://github.com/franciscaserrao" target="_blank">
+        <a
+          href="https://github.com/franciscaserrao"
+          target="_blank"
+          rel="noreferrer"
+        >
           GitHub
         </a>{" "}
         and hosted on{" "}
-        <a href="https://github.com/franciscaserrao" target="_blank">
+        <a
+          href="https://github.com/franciscaserrao"
+          target="_blank"
+          rel="noreferrer"
+        >
           Netlify
         </a>
       </p>
